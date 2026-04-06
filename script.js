b33020d21ec183ab41f44c93a96611a75ac41b6e
Jasper-Weiss <jasper.weiss@yahoo.com>
2026-04-06 20:39:37 +0000

    rawName to playerName

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-// add javascript here
+var rawName = prompt("Enter your name:");
+if (!rawName || rawName.trim() === "") {
+  rawName = "Player";
+}
+rawName = rawName.trim();
+var playerName =
+  rawName.charAt(0).toUpperCase() + rawName.slice(1).toLowerCase();
