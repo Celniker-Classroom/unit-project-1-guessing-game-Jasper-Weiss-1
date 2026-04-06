ee2c5547ed583f53809bbb537ee29079a5ce5ba4
Jasper-Weiss <jasper.weiss@yahoo.com>
2026-04-06 20:42:47 +0000

    Variable Reset

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ if (!rawName || rawName.trim() === "") {
 rawName = rawName.trim();
 var playerName =
   rawName.charAt(0).toUpperCase() + rawName.slice(1).toLowerCase();
+
+var answer = 0;
+var range = 3;
+var guesses = 0;
+var wins = 0;
+var totalWinningGuesses = 0;
+var scores = [];
+var roundStart = 0;
+var roundTimes = [];
+var fastestTime = null;
+var gameActive = false;
